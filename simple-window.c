a41d4b75868c2fc05a22850207169963ecbd24ad
victofeijo <victor.feijoa@gmail.com>
2016-03-15 13:43:12 -0300

    Created the simple window with no data

---
+++ simple-window.c
@@ -0,0 +1,29 @@
+#include <gtk/gtk.h>
+
+/* For compile the file with the right flags:
+ * gcc `pkg-config --cflags gtk+-3.0` -o simple-window simple-window.c `pkg-config --libs gtk+-3.0`
+ */
+
+static void
+activate (GtkApplication* app,
+          gpointer        user_data)
+{
+  GtkWidget *window;
+
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Simple Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 400, 400);
+  gtk_widget_show_all (window);
+}
+
+int main(int argc, char **argv) {
+  GtkApplication* app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
+}
